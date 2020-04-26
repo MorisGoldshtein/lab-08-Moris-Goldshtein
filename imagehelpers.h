dab7d202e775ee4feac134bfcdd54840a9fc1748
Jack Jeong <jackjeong98@gmail.com>
2020-04-26 14:51:44 -0400

    changed to zman's format and scaled by 5

--- imagehelpers.h
+++ imagehelpers.h
@@ -3,19 +3,11 @@
 const int MAX_H = 512;
 const int MAX_W = 512;
 
-
 void readImage(std::string filename, int image[MAX_H][MAX_W], int &height, int &width);
-
 void writeImage(std::string filename, int image[MAX_H][MAX_W], int height, int width);
-
-void task_a(std::string);
-
-void task_b(std::string);
-
-void task_c(std::string);
-
-void task_d(std::string);
-
-void task_e(std::string);
-
-void task_f(std::string);
+void invert(int width, int height, int img[MAX_H][MAX_W]);
+void inverthalf(int width, int height, int img[MAX_H][MAX_W]);
+void box(int width, int height, int img[MAX_H][MAX_W]);
+void frame(int width, int height, int img[MAX_H][MAX_W]);
+void scale(int width, int height, int img[MAX_H][MAX_W]);
+void pixelate(int width, int height, int img[MAX_H][MAX_W]);
